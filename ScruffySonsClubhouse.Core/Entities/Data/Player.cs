f52161821e0a6944e841497a44290d2dd854c754
Jeremy Miller <migimasa@gmail.com>
2020-05-27 10:56:00 -0500

    update player entity

--- ScruffySonsClubhouse.Core/Entities/Data/Player.cs
+++ ScruffySonsClubhouse.Core/Entities/Data/Player.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using ScruffySonsClubhouse.SharedKernel;
 
 namespace ScruffySonsClubhouse.Core.Entities.Data
 {
-    public class Player
+    public class Player : BaseGuidEntity
     {
         public string Name { get; set; }
         public List<Character> Characters { get; set; }
---
+++ ScruffySonsClubhouse.SharedKernel/BaseGuidEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScruffySonsClubhouse.SharedKernel
+{
+    public class BaseGuidEntity
+    {
+        public Guid Id { get; set; }
+        public List<BaseDomainEvent> Events = new List<BaseDomainEvent>();
+    }
+}
