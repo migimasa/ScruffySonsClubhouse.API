0d5dcfee2f14fff0bb7d057d299f080d0afd256b
Jeremy Miller <migimasa@gmail.com>
2020-07-01 06:28:49 -0500

    Set up Infrastructure project

---
+++ ScruffySonsClubhouse.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using ScruffySonsClubhouse.SharedKernel;
+
+namespace ScruffySonsClubhouse.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly IMediator _mediator;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options, IMediator mediator)
+            : base(options)
+        {
+            _mediator = mediator;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+            // ignore events if no dispatcher provided
+            if (_mediator == null) return result;
+
+            // dispatch events only is save was successful
+            var entitiesWithEvents = ChangeTracker.Entries<BaseEntity>().Select(e => e.Entity)
+                .Where(e => e.Events.Any()).ToArray();
+
+            foreach (var entity in entitiesWithEvents)
+            {
+                var events = entity.Events.ToArray();
+                entity.Events.Clear();
+                foreach (var domainEvent in events)
+                {
+                    await _mediator.Publish(domainEvent).ConfigureAwait(false);
+                }
+            }
+
+            return result;
+        }
+
+        public override int SaveChanges()
+        {
+            return SaveChangesAsync().GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ ScruffySonsClubhouse.Infrastructure/StartupSetup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ScruffySonsClubhouse.Infrastructure.Data;
+
+namespace ScruffySonsClubhouse.Infrastructure
+{
+    public static class StartupSetup
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString) =>
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString)); // will be created in web project root
+    }
+}
